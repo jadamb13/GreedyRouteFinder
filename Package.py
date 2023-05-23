d5f7e1729b442eaa60e484bcea507a0142967ecb
jbenn88 <jamesa.benn@gmail.com>
2023-05-22 22:08:08 -0400

    Initial commit

---
+++ Package.py
@@ -0,0 +1,14 @@
+
+class Package:
+
+    def __init__(self, package_id, address, city, state, zip,
+                 delivery_deadline, kilo, special_notes):
+        self.package_id = package_id
+        self.address = address
+        self.city = city
+        self.state = state
+        self.zip = zip
+        self.delivery_deadline = delivery_deadline
+        self. kilo = kilo
+        self.special_notes = special_notes
+
---
+++ Truck.py
@@ -0,0 +1,8 @@
+
+
+class Truck:
+    def __init__ (self, number_of_packages):
+        self.number_of_packages = number_of_packages
+
+    def get_number_of_packages(self):
+        return self.number_of_packages
