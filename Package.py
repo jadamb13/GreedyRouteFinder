ccd1f43332ac29ce8c3dac32d11e0565971a332b
jbenn88 <jamesa.benn@gmail.com>
2023-05-22 22:50:18 -0400

    Add ChainHashTable class with Hash Table class that can insert/search/remove items based on key-value pairs

--- Package.py
+++ Package.py
@@ -1,14 +1,14 @@
 
 class Package:
 
-    def __init__(self, package_id, address, city, state, zip,
-                 delivery_deadline, kilo, special_notes):
+    def __init__(self, package_id, address, city, state, zipcode,
+                 delivery_deadline, kilo, special_notes, delivery_status):
         self.package_id = package_id
         self.address = address
         self.city = city
         self.state = state
-        self.zip = zip
+        self.zipcode = zipcode
         self.delivery_deadline = delivery_deadline
         self. kilo = kilo
         self.special_notes = special_notes
-
+        self.delivery_status = delivery_status
